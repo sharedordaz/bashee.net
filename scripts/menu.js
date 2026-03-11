8a40a9357c670ba399b51e8d47f5dbaa91bd1f2c
sharedordaz <sharedordaz@live.com.mx>
2026-03-11 14:05:48 -0600

    headerspace script

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,9 @@
 function toggleMenu() {
     const navbar = document.getElementById('navbar');
     navbar.classList.toggle('nav-visible');
-  }
+
+    const headerSpace = document.querySelector('.headerSpace');
+    headerSpace.classList.toggle('headerSpaceExpanded');
+  } 
 
   //OTHER FUNCTION
